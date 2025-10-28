a66e6ce691c72caca59aa63fd52690a06b869a77
Pierre Grassin <pierre.312@hotmail.fr>
2025-10-28 18:44:07 +0100

    Replace axios usage with fetch for NUI posts

--- installfolder/database.sql
+++ installfolder/database.sql
@@ -1,6 +1,6 @@
 CREATE TABLE printer (
     id INT AUTO_INCREMENT PRIMARY KEY,
     image_name VARCHAR(255) NOT NULL,
-    image_link VARCHAR(255) NOT NULL
+    image_data LONGTEXT NOT NULL
 );
 
--- server/main.lua
+++ server/main.lua
@@ -6,28 +6,61 @@ end
 local resourceName = 'pl_printer'
 lib.versionCheck('pulsepk/pl_printer')
 
-RegisterServerEvent('pl_printer:insertImageData')
-AddEventHandler('pl_printer:insertImageData', function(imageUrl, amount)
-    local Player = getPlayer(source)
-    local account = Config.Print.Account
-    local TotalBill = Config.Print.Price*amount
-    if GetPlayerAccountMoney(Player,account,TotalBill) then
-        local imageName = imageUrl:match(".*/(.*)$")
-        AddItem(source,amount, imageName)
-        if imageUrl and amount then
-            MySQL.Async.execute('INSERT INTO printer (image_name, image_link) VALUES (@image_name, @image_link)', {
-                ['@image_name'] = tostring(imageName),
-                ['@image_link'] = imageUrl
-            }, function(rowsChanged)
-                
-            end)
-            RemovePlayerMoney(Player,account,TotalBill)
-            TriggerClientEvent('pl_printer:notification',source,Locale("Money_Removed") .. TotalBill,'success')
-        else
-            _debug('[DEBUG] '..' Invalid data received for image. '..'')
+math.randomseed(os.time())
+
+local function generateImageName(src, originalName)
+    local sanitized = 'print'
+
+    if type(originalName) == 'string' then
+        local baseName = originalName:gsub('%.%w+$', '')
+        baseName = baseName:gsub('%W+', '_'):gsub('_+', '_')
+        baseName = baseName:gsub('^_', ''):gsub('_$', '')
+        if baseName ~= '' then
+            sanitized = baseName
         end
+    end
+
+    local unique = string.format('%s_%d_%d_%d', sanitized, src or 0, os.time(), math.random(1000, 9999))
+    if #unique > 120 then
+        unique = unique:sub(1, 120)
+    end
+    return unique
+end
+
+RegisterServerEvent('pl_printer:insertImageData')
+AddEventHandler('pl_printer:insertImageData', function(imageData, amount, originalName)
+    local src = source
+    local Player = getPlayer(src)
+    if not Player then
+        print(('[%s] Unable to locate player for source %s when saving image'):format(resourceName, tostring(src)))
+        return
+    end
+    local account = Config.Print.Account
+    local copies = tonumber(amount)
+    copies = copies and math.floor(copies) or nil
+
+    if not copies or copies <= 0 then
+        TriggerClientEvent('pl_printer:notification', src, Locale("invalid_copies"), 'error')
+        return
+    end
+
+    if type(imageData) ~= 'string' or not imageData:find('^data:image') then
+        TriggerClientEvent('pl_printer:notification', src, Locale("invalid_image_data"), 'error')
+        return
+    end
+
+    local TotalBill = Config.Print.Price * copies
+    if GetPlayerAccountMoney(Player, account, TotalBill) then
+        local imageName = generateImageName(src, originalName)
+        AddItem(src, copies, imageName)
+        MySQL.Async.execute('INSERT INTO printer (image_name, image_data) VALUES (@image_name, @image_data)', {
+            ['@image_name'] = tostring(imageName),
+            ['@image_data'] = imageData
+        })
+        RemovePlayerMoney(Player, account, TotalBill)
+        TriggerClientEvent('pl_printer:notification', src, Locale("Money_Removed") .. TotalBill, 'success')
     else
-        TriggerClientEvent('pl_printer:notification',source,Locale("not_enough"),'error')
+        TriggerClientEvent('pl_printer:notification', src, Locale("not_enough"), 'error')
     end
 end)
 
@@ -36,11 +69,11 @@ RegisterServerEvent('pl_printer:fetchImageLink')
 AddEventHandler('pl_printer:fetchImageLink', function(imageName,playerSource)
     local hasItem = HasItem(playerSource)
     if not hasItem then return end
-    MySQL.Async.fetchScalar('SELECT image_link FROM printer WHERE image_name = @imageName', {
+    MySQL.Async.fetchScalar('SELECT image_data FROM printer WHERE image_name = @imageName', {
         ['@imageName'] = imageName
-    }, function(imageLink)
-        if imageLink then
-            TriggerClientEvent('pl_printer:showImage',playerSource,imageLink)
+    }, function(imageData)
+        if imageData then
+            TriggerClientEvent('pl_printer:showImage', playerSource, imageData)
         else
             _debug('[DEBUG] '..' No Image Link Found for '..imageName..'')
         end
@@ -62,7 +95,7 @@ function AddItem(source, amount, imageName)
             TriggerClientEvent('inventory:client:ItemBox', src, QBCore.Shared.Items[Config.ItemName], "add")
         end
     elseif GetResourceState('ox_inventory') == 'started' then
-        exports.ox_inventory:AddItem(src,Config.ItemName,amount,imageName,false)
+        exports.ox_inventory:AddItem(src, Config.ItemName, amount, { type = imageName }, false)
     end
 end
 
@@ -71,7 +104,20 @@ AddEventHandler('onServerResourceStart', function()
         exports(Config.ItemName,function (event,item,inventory,slot,data)
             if event == 'usingItem' then
                 local item_metadata = exports.ox_inventory:GetSlot(inventory.id, slot)
-                TriggerEvent('pl_printer:fetchImageLink', item_metadata.metadata.type, inventory.id)
+                local metadata = item_metadata and item_metadata.metadata
+                local imageId
+
+                if type(metadata) == 'table' then
+                    imageId = metadata.type or metadata.id or metadata.image_name
+                elseif type(metadata) == 'string' then
+                    imageId = metadata
+                end
+
+                if imageId then
+                    TriggerEvent('pl_printer:fetchImageLink', imageId, inventory.id)
+                else
+                    print(('[%s] Unable to determine image id for ox_inventory item'):format(resourceName))
+                end
             end
         end)
     end
